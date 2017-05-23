1840924eeed054fd6cb6f24b9bf915a0c463f190
Jake Hickey <jacob.hickey@gmail.com>
2017-05-23 15:49:30 -0400

    Squashed commit of the following:
    commit 8eaafac975f9c61445a1c515e40c05cdb81574fc
    Author: Jake Hickey <jacob.hickey@gmail.com>
    Date:   Tue May 23 15:42:14 2017 -0400
    
        Use jsonStream to send nicknames
    
    commit af718314433547e2e29b1b0ad425e7d3d36caaa8
    Author: Jake Hickey <empty@cqdr.es>
    Date:   Tue May 23 15:38:35 2017 -0400
    
        gitignore vim swap
    
    commit 5508ce27ee2adb55047f001a357ac4c127e81644
    Author: Jake Hickey <empty@cqdr.es>
    Date:   Tue May 23 15:25:55 2017 -0400
    
        use json stream for client.js

--- client.js
+++ client.js
@@ -1,14 +1,20 @@
 const net = require('net');
+const jsonStream = require('duplex-json-stream');
+
+const nickname = process.argv[2];
 
 const socket = net.connect(8080);
+const stream = jsonStream(socket);
 
 socket.on('connect', () => {
   process.stdin.on('data', (data) => {
-    socket.write(data);
+    stream.write({nickname: nickname, message: data.toString()});
   });
 });
 
-socket.on('data', (data) => {
-  process.stdout.write(data);
+stream.on('data', (data) => {
+  process.stdout.write(data.nickname);
+  process.stdout.write('> ');
+  process.stdout.write(data.message);
 });
 
