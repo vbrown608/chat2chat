53f10eb14a873cfd7394c7ffb0942a96de5ffd66
vivian <vivian@eff.org>
2017-05-23 14:38:53 -0400

    server keeps track of multiple clients
    Using stream-set to keep track of multiple sockets and only broadcast to sockets that didn't submit the text.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const net = require('net');
+const streamSet = require('stream-set');
+
+const activeSockets = streamSet();
 
 const server = net.createServer(c => {
   console.log('connected');
 
+  activeSockets.add(c);
+
   c.on('data', data => {
-    c.write(data);
+    activeSockets.forEach( (c2) => {
+      if(c2 !== c) {
+        c2.write(data);
+      };
+    });
   });
 
   c.on('end', () => {
