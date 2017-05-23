5508ce27ee2adb55047f001a357ac4c127e81644
Jake Hickey <empty@cqdr.es>
2017-05-23 15:25:55 -0400

    use json stream for client.js

--- client.js
+++ client.js
@@ -1,14 +1,20 @@
 const net = require('net');
+const jsonStream = require('duplex-json-stream');
 
-const socket = net.connect(8080);
+const nickname = process.argv[2];
+
+const socket = jsonStream(net.connect(8080));
 
 socket.on('connect', () => {
   process.stdin.on('data', (data) => {
-    socket.write(data);
+    console.log('Got data: ' + data);
+    socket.write({nickname: nickname, message: data});
   });
 });
 
 socket.on('data', (data) => {
-  process.stdout.write(data);
+  process.stdout.write(data.username);
+  process.stdout.write('> ');
+  process.stdout.write(data.message);
 });
 
