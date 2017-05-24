aef622c048e5358db41a1d6ff73a344a4d37ae42
Jake Hickey <jacob.hickey@gmail.com>
2017-05-24 15:07:35 -0400

    Squashed commit of the following:
    commit f5584afe7a64f4f9a4a4d39937e99dd212359f12
    Author: Jake Hickey <jacob.hickey@gmail.com>
    Date:   Wed May 24 15:06:46 2017 -0400
    
        Add chat feature to p2p client

--- peer.js
+++ peer.js
@@ -1,11 +1,32 @@
+'use strict';
+
 const topology = require('fully-connected-topology');
+const jsonStream = require('duplex-json-stream');
+const streamSet = require('stream-set');
 
 const myaddr = process.argv[2];
-const peers = process.argv.slice(3);
+const peerAddrs = process.argv.slice(3);
+const peers = streamSet();
 
-const mytop = topology(myaddr, peers);
+const mytop = topology(myaddr, peerAddrs);
 
-mytop.on('connection', (conn, peer) => {
-    console.log('mytop is connected to', peer);
+function writeAll(data) {
+  for (let stream of peers.streams) {
+    stream.write(data);
+  }
+}
+
+process.stdin.on('data', (data) => {
+  writeAll({nickname: myaddr, message: data.toString()});
 });
 
+mytop.on('connection', (conn, peerAddr) => {
+  console.log('mytop is connected to', peerAddr);
+  const peerJsonStream = jsonStream(conn);
+  peers.add(peerJsonStream);
+  peerJsonStream.on('data', (data) => {
+    process.stdout.write(data.nickname);
+    process.stdout.write('> ');
+    process.stdout.write(data.message);
+  });
+});
