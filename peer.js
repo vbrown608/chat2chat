e97783d72dc2b388293644a00717d9f471dda89d
vivian <vivian@eff.org>
2017-05-24 15:46:06 -0400

    Implement gossip protocol for p2p chat
    Accomodates a wider variety of topology by passing along messages to all clients.

--- peer.js
+++ peer.js
@@ -11,14 +11,37 @@ const peers = streamSet();
 
 const mytop = topology(myaddr, peerAddrs);
 
+var seq = 0;
+const id = Math.random();
+const peerSeqs = {};
+
 function writeAll(data) {
   for (let stream of peers.streams) {
     stream.write(data);
   }
 }
 
+function notSeen(data) {
+  let peerSeq = peerSeqs[data.id];
+  if (peerSeq !== undefined) {
+    return data.seq > peerSeq;
+  }
+  return true;
+}
+
+function updatePeerSeq(data) {
+  peerSeqs[data.id] = data.seq;
+}
+
 process.stdin.on('data', (data) => {
-  writeAll({nickname: nickname, message: data.toString()});
+  writeAll({
+    nickname: nickname,
+    message: data.toString(),
+    seq: seq,
+    id: id
+  });
+
+  seq += 1;
 });
 
 mytop.on('connection', (conn, peerAddr) => {
@@ -26,8 +49,16 @@ mytop.on('connection', (conn, peerAddr) => {
   const peerJsonStream = jsonStream(conn);
   peers.add(peerJsonStream);
   peerJsonStream.on('data', (data) => {
-    process.stdout.write(data.nickname);
-    process.stdout.write('> ');
-    process.stdout.write(data.message);
+    if (notSeen(data)) {
+      printMessage(data);
+      writeAll(data);
+      updatePeerSeq(data);
+    }
   });
 });
+
+function printMessage(data) {
+  process.stdout.write(data.nickname);
+  process.stdout.write('> ');
+  process.stdout.write(data.message);
+}
