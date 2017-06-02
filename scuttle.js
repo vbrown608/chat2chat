e1817834f4b03c3091d030d9e2804407158e8da4
vivian <vivian@eff.org>
2017-06-01 21:19:55 -0400

    Keys are strings - they need padding

--- scuttle.js
+++ scuttle.js
@@ -1,19 +1,26 @@
 const level = require('level');
 const nickname = process.argv[2];
 
+const db = level(nickname + '.db');
+
+function pad(n, l, p) {
+  n = n + "";
+  return n.length >= l ? n : new Array(l - n.length + 1).join(p) + n;
+}
+
 function encodeKey(peer, seq) {
-  return peer*1000 + seq;
+  return pad(peer, 4, '0') + pad(seq, 4, '0');
 }
 
 function decodeKey(key) {
   return {
-    peer: key/1000,
-    seq: key%1000
+    peer: parseInt(key.slice(0, 4)),
+    seq: parseInt(key.slice(4))
   }
 }
 
 function peerMaxKey(peer) {
-  return (peer + 1) * 1000;
+  return pad(peer + 1, 4, '0');
 }
 
 // Get the highest seq for each peer
@@ -49,6 +56,4 @@ db.put(encodeKey(2, 1), 'onion');
 db.put(encodeKey(2, 2), 'noodles');
 db.put(encodeKey(2, 3), 'fusilli');
 
-getPeerUpdates(2, 0);
-// getAllUpdates([
-    // { peer: 0, -1
+getPeerUpdates(0, 0);
